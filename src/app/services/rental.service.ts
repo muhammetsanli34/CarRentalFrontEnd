f0a79c0e2ec2c3753e29ab86294060d3cf220677
sanlimamy <69751354+sanlimamy@users.noreply.github.com>
2021-03-29 22:11:17 +0300

    CarRentalFrontEnd

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Rental } from '../models/rental';
 import { RentalResponseModel } from '../models/rentalResponseModel';
 
 @Injectable({
